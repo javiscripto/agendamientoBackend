4d20c3dd826b95b5bafedc343931e92afd558639
javier ismael <javier.mecker94@gmail.com>
2025-04-24 19:57:59 -0400

    update: se agrega middleware para validar solapamiento entre fechas antes de guardar en db

--- src/mongo/models/rental.models.js
+++ src/mongo/models/rental.models.js
@@ -3,16 +3,39 @@ import mongoose from "mongoose";
 const rentalCollection = "Rental";
 
 //este schema deberia tener como uno de sus atributos la fecha de inicio y fin de arriendo del apartamento
-const rentalSchema = new mongoose.Schema({
-  property: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Property",
-    required: true,
+const rentalSchema = new mongoose.Schema(
+  {
+    property: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
+    // tentant: { type: mongoose.Schema.Types.ObjectId, ref: "Gest" },
+    days: { type: Number, required: true },
+    price: { type: Number, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
   },
-  // tentant: { type: mongoose.Schema.Types.ObjectId, ref: "Gest" },
-  //price: { type: Number, required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
+  {
+    timestamps: true,
+  },
+);
+
+//middleware para verificar disponibilidad
+rentalSchema.pre("save", async function(next) {
+  const overlappingRental = await mongoose.model("Rental").findOne({
+    property: this.property,
+    $or: [
+      // { startDate: { $lt: this.endDate }, endDate: { $gt: this.startDate } },
+      { startDate: { $lte: this.startDate }, endDate: { $gte: this.endDate } },
+    ],
+  });
+
+  if (overlappingRental) {
+    throw new Error("la propiedad se encuentra reservada");
+  }
+
+  next();
 });
 
 const rentalModel = mongoose.model(rentalCollection, rentalSchema);
