9e67d9b0eb581d2dbd657696997bb7ca3baeac2d
javier ismael <javier.mecker94@gmail.com>
2025-04-24 19:54:26 -0400

    update: se agrega virtual para obtener las rentas de cada propiedad

--- src/mongo/models/apartment.models.js
+++ src/mongo/models/apartment.models.js
@@ -2,14 +2,41 @@ import mongoose from "mongoose";
 
 const propertyCollection = "Property";
 
-const propertySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  images: [{ type: String }],
-  isAvailable: { type: Boolean, default: true },
+const propertySchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    isAvailable: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+//virtuals para obtener todos los arriendos de un departamento
+propertySchema.virtual("rentals", {
+  ref: "Rental",
+  localField: "_id",
+  foreignField: "property",
+  justOne: false,
 });
 
+//metodo para obtener fechas reservadas de un departamento
+propertySchema.methods.getBookedDates = async function() {
+  const rentals = await mongoose
+    .model("Rental")
+    .find({
+      property: this._id,
+      endDate: { $gte: new Date() },
+    })
+    .select("startDate endDate -_id");
+
+  return rentals;
+};
 const apartmentModel = mongoose.model(propertyCollection, propertySchema);
 
 export default apartmentModel;
