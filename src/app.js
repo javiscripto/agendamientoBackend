9fdea614720ab47a334d90c254fc9caf590d6c6e
javiscripto <javier.mecker94@gmail.com>
2025-04-23 20:57:53 -0400

    configuracion de cors para consultas externas

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { engine } from "express-handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 // 2. Módulos internos (como DB, logger, routers, etc.)
@@ -14,19 +15,20 @@ import logger from "../logger/winston.js";
 import paymentRouter from "./routes/payment.router.js";
 import apartmentRouter from "./routes/apartment.router.js";
 
-
 // 4. Configuración de Express
 const app = express();
 const port = process.env.PORT || 3000;
 
 // 5. Conectar a la base de datos y levantar el servidor
-connectDb().then(() => {
-  app.listen(port, () => {
-    logger.info(`Servidor corriendo en el puerto ${port}`);
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      logger.info(`Servidor corriendo en el puerto ${port}`);
+    });
+  })
+  .catch((err) => {
+    logger.error("Error al conectar con la base de datos:", err);
   });
-}).catch((err) => {
-  logger.error("Error al conectar con la base de datos:", err);
-});
 
 // 6. Configuración de __dirname para rutas relativas
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +40,7 @@ app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 
 // 8. Middlewares de Express
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
