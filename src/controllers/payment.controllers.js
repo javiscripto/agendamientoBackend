778e1dbbdc7b9c3d34e789a89abdbc0078211ede
javier ismael <javier.mecker94@gmail.com>
2025-05-01 18:27:08 -0400

    cambio metodo http de la ruta api/payment/checkout de get a post para recibir datos desde el body del front

--- src/controllers/payment.controllers.js
+++ src/controllers/payment.controllers.js
@@ -1,31 +1,16 @@
-import Stripe from "stripe";
+import PaymentService from "../services/payment.service.js ";
 import dotenv from "dotenv";
 dotenv.config();
 
-const stripe = new Stripe(process.env.STRIPE_KEY);
+const paymentService = new PaymentService();
 
 export const createSesion = async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          product_data: {
-            name: "departamento 1",
-            description: "descripcion del departamento",
-          },
-
-          currency: "usd",
-          unit_amount: 10000, //100.00
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000/payment/cancel",
-  });
-  const url = session.url;
-  return res.redirect(url);
+  try {
+    const response = await paymentService.createSession(req.body);
+    res.json({ url: response });
+  } catch (error) {
+    res.status(500).json({ status: "error", error: error.message });
+  }
 };
 
 //----------------------------------------------
